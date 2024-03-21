5aa1fe89319917549832290f8951328416c81014
Tanongsak Intean <71477852+tanongsakintean@users.noreply.github.com>
2024-03-22 03:18:29 +0700

    set body

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,14 +1,41 @@
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions, StyleSheet} from 'react-native';
 import React from 'react';
 import HeaderHome from '../components/header/HeaderHome';
+import {SwiperFlatList} from 'react-native-swiper-flatlist';
+const colors = ['tomato', 'thistle', 'skyblue', 'teal'];
 
 const HomeScreen = () => {
   return (
-    <View>
-      <HeaderHome />
-      <Text className="text-blue-500">HomeScreen</Text>
+    <View style={styles.container}>
+      <SwiperFlatList
+        autoplay
+        autoplayDelay={2}
+        autoplayLoop
+        index={2}
+        showPagination>
+        <View style={[styles.child, {backgroundColor: 'tomato'}]}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={[styles.child, {backgroundColor: 'thistle'}]}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={[styles.child, {backgroundColor: 'skyblue'}]}>
+          <Text style={styles.text}>3</Text>
+        </View>
+        <View style={[styles.child, {backgroundColor: 'teal'}]}>
+          <Text style={styles.text}>4</Text>
+        </View>
+      </SwiperFlatList>
     </View>
   );
 };
+
+const {width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'white'},
+  child: {width, justifyContent: 'center'},
+  text: {fontSize: width * 0.5, textAlign: 'center'},
+});
 
 export default HomeScreen;
