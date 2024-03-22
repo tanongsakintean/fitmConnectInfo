cd71c3ad6f5e7b4cd02cdd002ce332a6a8f5c005
Tanongsak Intean <71477852+tanongsakintean@users.noreply.github.com>
2024-03-23 03:15:04 +0700

    set gradient

--- src/screens/CourseScreen.jsx
+++ src/screens/CourseScreen.jsx
@@ -1,17 +1,23 @@
 import {Text, View} from 'react-native';
 import React, {Component} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
 
 export class CourseScreen extends Component {
   render() {
     return (
       <View>
-        <View>
-          <View className=" flex flex-row justify-evenly">
-            <Text
-              className=" text-5xl text-white rounded-md  p-5 "
-              style={{fontFamily: 'Kanit-Mediu'}}>
-              ITI
-            </Text>
+        <View className=" flex flex-row justify-evenly">
+          <View>
+            <LinearGradient
+              start={{x: -1, y: 0}}
+              end={{x: 1, y: 0}}
+              colors={['#020024', '#090979', '#00d4ff']}>
+              <Text
+                className=" text-5xl text-white rounded-md   p-5  "
+                style={{fontFamily: 'Kanit-Mediu'}}>
+                ITI
+              </Text>
+            </LinearGradient>
           </View>
         </View>
       </View>
